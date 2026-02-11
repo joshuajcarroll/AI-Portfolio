9911dba07066c33ad6b29cb70aa1b41825bb4904
Joshua Carroll <carrolljosh1976@gmail.com>
2026-02-11 03:11:18 -0500

    finished project

--- components/DisableDraftMode.tsx
+++ components/DisableDraftMode.tsx
@@ -12,7 +12,7 @@ export function DisableDraftMode() {
   const environment = useDraftModeEnvironment();
 
   // Only show the disable draft mode button when outside of Presentation Tool
-  if (environment !== "live" && environment !== "unknown") {
+  if (environment === "live" || environment === "unknown") {
     return null;
   }
 
